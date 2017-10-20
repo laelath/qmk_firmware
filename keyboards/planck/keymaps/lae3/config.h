0fea68107538af5f0bde8a27515c2f902b717731
Justin Frank <jf.laelath@gmail.com>
2017-10-19 20:05:41 -0700

    Changed mousekey sensitivities

---
+++ keyboards/planck/keymaps/lae3/config.h
@@ -0,0 +1,15 @@
+#ifndef LAE3_KEYMAP_H
+#define LAE3_KEYMAP_H
+
+#include "../../config.h"
+
+#define PREVENT_STUCK_MODIFIERS
+
+#define MOUSEKEY_DELAY              100
+#define MOUSEKEY_INTERVAL           20
+#define MOUSEKEY_MAX_SPEED          5
+#define MOUSEKEY_TIME_TO_MAX        50
+#define MOUSEKEY_WHEEL_MAX_SPEED    1
+#define MOUSEKEY_WHEEL_TIME_TO_MAX  100
+
+#endif
